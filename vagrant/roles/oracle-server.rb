1eba0b8f8bb0ceffb61037f8a8cb19bf342cb907
Eduardo Diaz <iamedu@Gmail.com>
2013-03-07 13:50:17 -0600

    Testing oracle xe

--- vagrant/roles/oracle-server.rb
+++ vagrant/roles/oracle-server.rb
@@ -2,15 +2,17 @@ name "oracle-server"
 description "Install Oracle server"
 
 run_list(
-	"recipe[oracle]"
+	"recipe[oracle]",
+	"recipe[oracle-xe]"
 )
 
 
 override_attributes "sysctl" => {
 	"is_oracle" => true
-},
-	"oracle" => {
+}, "oracle" => {
 	"dbas" => ["vagrant"],
 	"version" => 11,
 	"processes" => 512
+}, "oraclexe" => {
+	"password" => "password"
 }
